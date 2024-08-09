fde5b82e5b63640215e109147f908d965917423e
agolovanov <alexandergolovanovvla@gmail.com>
2024-08-10 00:02:30 +0300

    .

--- myproject/catalog/urls.py
+++ myproject/catalog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('product/<int:pk>/', views.product_detail_view, name='product_detail'),
     path('', views.index_view, name='index'),
+    path('create/', views.create_product, name='create_product'),
+    path('', views.create_product, name='create_product'),
+    path('list/', views.product_list, name='product_list'),
+    path('', views.product_list, name='product_list'),
 ]
--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+
+from .forms import ProductForm 
 from .models import Product
+from django.core.paginator import Paginator
 
 def home(request):
     return render(request, 'catalog/home.html')
@@ -34,3 +37,23 @@ def product_detail_view(request, pk):
     context = {'object': product}
 
     return render(request, 'catalog/product_detail.html', context)
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+       form = ProductForm()
+    
+    return render(request, 'catalog/create_product.html', {'form': form})
+
+def product_list(request):
+    product_list = Product.objects.all()
+    paginator = Paginator(product_list, 10) 
+
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(page_number)
+
+    return render(request, 'catalog/product_list.html', {'page_obj': page_obj})
