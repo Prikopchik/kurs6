93c91d3aa38fdc27b2d16ffbf7925d446ecfd430
agolovanov <alexandergolovanovvla@gmail.com>
2024-08-09 22:58:52 +0300

    .

--- myproject/catalog/urls.py
+++ myproject/catalog/urls.py
@@ -5,4 +5,5 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
